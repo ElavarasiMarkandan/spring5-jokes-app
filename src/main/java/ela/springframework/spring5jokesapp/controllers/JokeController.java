9d35cf55feffb5b80d1060adab0bc6addc317bf3
Elavarasi Markandan <elavarasimarkandan@gmail.com>
2021-05-17 18:37:30 -0600

    First Commit

---
+++ src/main/java/ela/springframework/spring5jokesapp/controllers/JokeController.java
@@ -0,0 +1,21 @@
+package ela.springframework.spring5jokesapp.controllers;
+
+import ela.springframework.spring5jokesapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("randomJoke",jokeService.getJokes());
+        return "index";
+
+    }
+}
---
+++ src/main/java/ela/springframework/spring5jokesapp/services/JokeService.java
@@ -0,0 +1,5 @@
+package ela.springframework.spring5jokesapp.services;
+
+public interface JokeService {
+    String getJokes();
+}
